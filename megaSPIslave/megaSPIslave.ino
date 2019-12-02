4e22c1902da469d1f2a5b49a45bddaf54ef2c2ad
cottjr <carl.ott.jr@gmail.com>
2019-12-02 07:00:37 -0600

    Back to working baseline of polled arduinoMathServer implementation. Fixed arduino side code after garbled with git.

--- megaSPIslave/megaSPIslave.ino
+++ megaSPIslave/megaSPIslave.ino
@@ -27,10 +27,9 @@
 
 unsigned char hello[] = {'H','e','l','l','o',' ',
                          'R','a','s','p','i','\n'};
-volatile unsigned char receiveBuffer[5];
-volatile unsigned char dat;                         
-volatile byte marker = 0;
-volatile unsigned long SPIwdPriorMillis;
+unsigned char receiveBuffer[5];
+unsigned char dat;                         
+byte marker = 0;
  
 #define digTP26 26  // digital test point #1 (pin 26) // CPU status monitoring via o'scope
 #define digTP27 27  // digital test point #2 (pin 27) // CPU status monitoring via o'scope
@@ -43,20 +42,20 @@ volatile unsigned long SPIwdPriorMillis;
  and one to receive  the results 
  ***************************************************************/
 
-volatile union       
+union       
   {
   int p1Int;
   unsigned char  p1Char [2];
   } p1Buffer;
 
-volatile union       
+union       
   {
   int p2Int;
   unsigned char p2Char [2];
   } p2Buffer;
 
 
-volatile union       
+union       
   {
   int resultInt;
   unsigned char  resultChar [2];
@@ -80,19 +79,14 @@ void setup (void)
 
   Serial.println("entering setup");
 
-  // Digital Test Points
+    // Digital Test Points
   pinMode (digTP26, OUTPUT);
   pinMode (digTP27, OUTPUT);
   digitalWrite(digTP26, LOW);
   digitalWrite(digTP27, LOW);
 
-  SPIwdPriorMillis = millis();  // initialize the SPI watchdog time counter
-
   pinMode(MISO, OUTPUT);
   SPCR |= _BV(SPE);
-
-  // turn on SPI interrupts
-  SPI.attachInterrupt();
 
   Serial.println("completed setup");
 
@@ -111,18 +105,30 @@ void setup (void)
  Loop until the SPI End of Transmission Flag (SPIF) is set
  indicating a byte has been received.  When a byte is
  received, call the spiHandler function.
-
- Version 3
- trigger interrupts per this template
-   https://roboticsbackend.com/raspberry-pi-master-arduino-uno-slave-spi-communication-with-wiringpi/
 ****************************************************************/
 
-// Purpose
-//  Assess arrival of another byte via SPI
-// Algorithm
-//  Ignore it, Reset the transfer protocol state machine, or add the latest transferred byte to an 'being catured' buffer
+void loop (void)
+{
+
+  if((SPSR & (1 << SPIF)) != 0)
+  {
+    // // Serial.println(marker);
+    // SPDR = hello[marker];
+    // marker++;
+   
+    // if(marker > sizeof(hello))
+    // {
+    //   Serial.println("done");
+    //   marker = 0;
+    // }  
+    digitalWrite(digTP26, HIGH);
+    spiHandler();
+    digitalWrite(digTP26, LOW);    
+  }
+}
+
 /***************************************************************  
-Derived from polled example code spiHandler()
+ spiHandler
    Uses the marker variable to keep track current position in the
    incoming data packet and execute accordingly
    0   - wait for to receive start byte - once received send
@@ -137,143 +143,52 @@ Derived from polled example code spiHandler()
    7   - transmit the second byte of of the result and reset
          the marker   
 ****************************************************************/
-ISR (SPI_STC_vect)
+
+
+void spiHandler()
 {
-    digitalWrite(digTP26, HIGH);
-
-    // reset the SPI listener state machine if the prior transfer has clearly taken too long or was prematurely cancelled
-    // ie. this will force the listener to wait for another initial 'start new transfer' marker byte
-    // choose threshold based on empirical timing observation by oscilloscope plus a little margin
-    if ( (millis() - SPIwdPriorMillis) > 5)
+  switch (marker)
+  {
+  case 0:
+    dat = SPDR;
+    if (dat == 'c')
     {
-      marker=0;
-    }
+      SPDR = 'a';
+      marker++;
+    } 
+    break;    
+  case 1:
+    receiveBuffer[marker-1] = SPDR;
+    marker++;
+    break;
+  case 2:
+    receiveBuffer[marker-1] = SPDR;
+    marker++;
+    break;
+  case 3:
+    receiveBuffer[marker-1] = SPDR;
+    marker++;
+    break;
+  case 4:
+    receiveBuffer[marker-1] = SPDR;
+    marker++;
+    break;
+  case 5:
+    receiveBuffer[marker-1] = SPDR;
+    marker++;
+    executeCommand();
+    SPDR = resultBuffer.resultChar[0];    
+    break;    
+  case 6:
+    marker++;
+    SPDR = resultBuffer.resultChar[1]; 
+    break;   
+  case 7:
+    dat = SPDR;
+    marker=0;
+  }
 
-    switch (marker)
-    {
-    case 0:
-      SPIwdPriorMillis = millis();
-      dat = SPDR;
-      if (dat == 'c')
-      {
-        SPDR = 'a';
-        marker++;
-      } 
-      break;    
-    case 1:
-      receiveBuffer[marker-1] = SPDR;
-      marker++;
-      break;
-    case 2:
-      receiveBuffer[marker-1] = SPDR;
-      marker++;
-      break;
-    case 3:
-      receiveBuffer[marker-1] = SPDR;
-      marker++;
-      break;
-    case 4:
-      receiveBuffer[marker-1] = SPDR;
-      marker++;
-      break;
-    case 5:
-      receiveBuffer[marker-1] = SPDR;
-      marker++;
-      executeCommand();
-      SPDR = resultBuffer.resultChar[0];    
-      break;    
-    case 6:
-      marker++;
-      SPDR = resultBuffer.resultChar[1]; 
-      break;   
-    case 7:
-      dat = SPDR;
-      marker=0;
-    }
-    digitalWrite(digTP26, LOW);    
 }
-
-void loop (void)
-{
-
-  // if((SPSR & (1 << SPIF)) != 0)
-  // {
-  //   // // Serial.println(marker);
-  //   // SPDR = hello[marker];
-  //   // marker++;
-   
-    // if(marker > sizeof(hello))
-    // {
-    //   Serial.println("done");
-    //   marker = 0;
-    // }  
-    // digitalWrite(digTP26, HIGH);
-    // spiHandler();
-    // digitalWrite(digTP26, LOW);    
-}
-
-// /***************************************************************  
-//  spiHandler
-//    Uses the marker variable to keep track current position in the
-//    incoming data packet and execute accordingly
-//    0   - wait for to receive start byte - once received send
-//          the acknowledge byte
-//    1   - the command to add or subtract
-//    2-5 - two integer parameters to be added or subtracted
-//        - when the last byte (5) is received, call the
-//          executeCommand function and load the first byte of the
-//          result into SPDR
-//    6   - transmit the first byte of the result and load the 
-//          second byte into SPDR
-//    7   - transmit the second byte of of the result and reset
-//          the marker   
-// ****************************************************************/
-
-
-// void spiHandler()
-// {
-//   switch (marker)
-//   {
-//   case 0:
-//     dat = SPDR;
-//     if (dat == 'c')
-//     {
-//       SPDR = 'a';
-//       marker++;
-//     } 
-//     break;    
-//   case 1:
-//     receiveBuffer[marker-1] = SPDR;
-//     marker++;
-//     break;
-//   case 2:
-//     receiveBuffer[marker-1] = SPDR;
-//     marker++;
-//     break;
-//   case 3:
-//     receiveBuffer[marker-1] = SPDR;
-//     marker++;
-//     break;
-//   case 4:
-//     receiveBuffer[marker-1] = SPDR;
-//     marker++;
-//     break;
-//   case 5:
-//     receiveBuffer[marker-1] = SPDR;
-//     marker++;
-//     executeCommand();
-//     SPDR = resultBuffer.resultChar[0];    
-//     break;    
-//   case 6:
-//     marker++;
-//     SPDR = resultBuffer.resultChar[1]; 
-//     break;   
-//   case 7:
-//     dat = SPDR;
-//     marker=0;
-//   }
-
-// }
 
 /***************************************************************  
  executeCommand
