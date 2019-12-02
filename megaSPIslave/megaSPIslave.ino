e2612cef29b6cb816c232ba1f139a99af00ef61a
cottjr <carl.ott.jr@gmail.com>
2019-12-01 20:10:00 -0600

    Working baseline of arduino math server, after converted from ardunio as polled implementation to interrupt driven implementation.

--- megaSPIslave/megaSPIslave.ino
+++ megaSPIslave/megaSPIslave.ino
@@ -26,10 +26,12 @@
 
 unsigned char hello[] = {'H','e','l','l','o',' ',
                          'R','a','s','p','i','\n'};
-unsigned char receiveBuffer[5];
-unsigned char dat;                         
-byte marker = 0;
+volatile unsigned char receiveBuffer[5];
+volatile unsigned char dat;                         
+volatile byte marker = 0;
  
+#define digTP26 26  // digital test point #1 (pin 26) // CPU status monitoring via o'scope
+#define digTP27 27  // digital test point #2 (pin 27) // CPU status monitoring via o'scope
 
 /*************************************************************
  Unions allow variables to occupy the same memory space a 
@@ -39,20 +41,20 @@ byte marker = 0;
  and one to receive  the results 
  ***************************************************************/
 
-union       
+volatile union       
   {
   int p1Int;
   unsigned char  p1Char [2];
   } p1Buffer;
 
-union       
+volatile union       
   {
   int p2Int;
   unsigned char p2Char [2];
   } p2Buffer;
 
 
-union       
+volatile union       
   {
   int resultInt;
   unsigned char  resultChar [2];
@@ -73,6 +75,12 @@ void setup (void)
                           // e.g. https://www.quora.com/What-is-the-baud-rate-and-why-does-Arduino-have-a-baud-rate-of-9-600
 
   Serial.println("entering setup");
+
+  // Digital Test Points
+  pinMode (digTP26, OUTPUT);
+  pinMode (digTP27, OUTPUT);
+  digitalWrite(digTP26, LOW);
+  digitalWrite(digTP27, LOW);
 
   pinMode(MISO, OUTPUT);
   SPCR |= _BV(SPE);
@@ -103,34 +111,12 @@ void setup (void)
    https://roboticsbackend.com/raspberry-pi-master-arduino-uno-slave-spi-communication-with-wiringpi/
 ****************************************************************/
 
-// SPI interrupt routine
-ISR (SPI_STC_vect)
-{
-  byte c = SPDR;
-  SPDR = c+12;
-}  // end of interrupt service routine (ISR) for SPI
-
-
-void loop (void)
-{
-
-  // if((SPSR & (1 << SPIF)) != 0)
-  // {
-  //   // // Serial.println(marker);
-  //   // SPDR = hello[marker];
-  //   // marker++;
-   
-  //   // if(marker > sizeof(hello))
-  //   // {
-  //   //   Serial.println("done");
-  //   //   marker = 0;
-  //   // }  
-  //   spiHandler();
-  // }
-}
-
+// Purpose
+//  Assess arrival of another byte via SPI
+// Algorithm
+//  Ignore it, Reset the transfer protocol state machine, or add the latest transferred byte to an 'being catured' buffer
 /***************************************************************  
- spiHandler
+Derived from polled example code spiHandler()
    Uses the marker variable to keep track current position in the
    incoming data packet and execute accordingly
    0   - wait for to receive start byte - once received send
@@ -145,52 +131,133 @@ void loop (void)
    7   - transmit the second byte of of the result and reset
          the marker   
 ****************************************************************/
-
-
-void spiHandler()
+ISR (SPI_STC_vect)
 {
-  switch (marker)
-  {
-  case 0:
-    dat = SPDR;
-    if (dat == 'c')
+    digitalWrite(digTP26, HIGH);
+    switch (marker)
     {
-      SPDR = 'a';
+    case 0:
+      dat = SPDR;
+      if (dat == 'c')
+      {
+        SPDR = 'a';
+        marker++;
+      } 
+      break;    
+    case 1:
+      receiveBuffer[marker-1] = SPDR;
       marker++;
-    } 
-    break;    
-  case 1:
-    receiveBuffer[marker-1] = SPDR;
-    marker++;
-    break;
-  case 2:
-    receiveBuffer[marker-1] = SPDR;
-    marker++;
-    break;
-  case 3:
-    receiveBuffer[marker-1] = SPDR;
-    marker++;
-    break;
-  case 4:
-    receiveBuffer[marker-1] = SPDR;
-    marker++;
-    break;
-  case 5:
-    receiveBuffer[marker-1] = SPDR;
-    marker++;
-    executeCommand();
-    SPDR = resultBuffer.resultChar[0];    
-    break;    
-  case 6:
-    marker++;
-    SPDR = resultBuffer.resultChar[1]; 
-    break;   
-  case 7:
-    dat = SPDR;
-    marker=0;
-  }
-
+      break;
+    case 2:
+      receiveBuffer[marker-1] = SPDR;
+      marker++;
+      break;
+    case 3:
+      receiveBuffer[marker-1] = SPDR;
+      marker++;
+      break;
+    case 4:
+      receiveBuffer[marker-1] = SPDR;
+      marker++;
+      break;
+    case 5:
+      receiveBuffer[marker-1] = SPDR;
+      marker++;
+      executeCommand();
+      SPDR = resultBuffer.resultChar[0];    
+      break;    
+    case 6:
+      marker++;
+      SPDR = resultBuffer.resultChar[1]; 
+      break;   
+    case 7:
+      dat = SPDR;
+      marker=0;
+    }
+    digitalWrite(digTP26, LOW);    
 }
+
+void loop (void)
+{
+
+  // if((SPSR & (1 << SPIF)) != 0)
+  // {
+  //   // // Serial.println(marker);
+  //   // SPDR = hello[marker];
+  //   // marker++;
+   
+    // if(marker > sizeof(hello))
+    // {
+    //   Serial.println("done");
+    //   marker = 0;
+    // }  
+    // digitalWrite(digTP26, HIGH);
+    // spiHandler();
+    // digitalWrite(digTP26, LOW);    
+}
+
+// /***************************************************************  
+//  spiHandler
+//    Uses the marker variable to keep track current position in the
+//    incoming data packet and execute accordingly
+//    0   - wait for to receive start byte - once received send
+//          the acknowledge byte
+//    1   - the command to add or subtract
+//    2-5 - two integer parameters to be added or subtracted
+//        - when the last byte (5) is received, call the
+//          executeCommand function and load the first byte of the
+//          result into SPDR
+//    6   - transmit the first byte of the result and load the 
+//          second byte into SPDR
+//    7   - transmit the second byte of of the result and reset
+//          the marker   
+// ****************************************************************/
+
+
+// void spiHandler()
+// {
+//   switch (marker)
+//   {
+//   case 0:
+//     dat = SPDR;
+//     if (dat == 'c')
+//     {
+//       SPDR = 'a';
+//       marker++;
+//     } 
+//     break;    
+//   case 1:
+//     receiveBuffer[marker-1] = SPDR;
+//     marker++;
+//     break;
+//   case 2:
+//     receiveBuffer[marker-1] = SPDR;
+//     marker++;
+//     break;
+//   case 3:
+//     receiveBuffer[marker-1] = SPDR;
+//     marker++;
+//     break;
+//   case 4:
+//     receiveBuffer[marker-1] = SPDR;
+//     marker++;
+//     break;
+//   case 5:
+//     receiveBuffer[marker-1] = SPDR;
+//     marker++;
+//     executeCommand();
+//     SPDR = resultBuffer.resultChar[0];    
+//     break;    
+//   case 6:
+//     marker++;
+//     SPDR = resultBuffer.resultChar[1]; 
+//     break;   
+//   case 7:
+//     dat = SPDR;
+//     marker=0;
+//   }
+
+// }
 
 /***************************************************************  
  executeCommand
