96c40482fb5c8154add17adf84a0dd4fc7628511
cottjr <carl.ott.jr@gmail.com>
2019-12-03 07:47:58 -0600

    More comments & dead code cleanup for piSPImaster.cpp

--- piSPImaster/piSPImaster.cpp
+++ piSPImaster/piSPImaster.cpp
@@ -40,10 +40,7 @@ using namespace std;
 /**********************************************************
 Declare Global Variables
 ***********************************************************/
-
 int fd;
-unsigned char hello[] = {'H','e','l','l','o',' ',
-                           'A','r','d','u','i','n','o'};
 unsigned char result;
 
 
@@ -56,21 +53,10 @@ int results;
 /**********************************************************
 Declare Functions
 ***********************************************************/
-
 int spiTxRx(unsigned char txDat);
 int sendCommand(char i, int j, int k);
 
 
-/**********************************************************
-Main
-  Setup SPI
-    Open file spidev0.0 (chip enable 0) for read/write 
-      access with the file descriptor "fd"
-    Configure transfer speed (1MkHz)
-  Start an endless loop that repeatedly sends the characters
-    in the hello[] array to the Ardiuno and displays
-    the returned bytes
-***********************************************************/
 
 int main (void)
 {
@@ -82,12 +68,12 @@ int main (void)
   digitalWrite(6, HIGH); // set GPIO.6, high, to enable outputs on the SPI level translater from Raspberry to Arduino Mega
 
 
-/**********************************************************
-Setup SPI
-Open file spidev0.0 (chip enable 0) for read/write access
-with the file descriptor "fd"
-Configure transfer speed (1MkHz)
-***********************************************************/
+  /**********************************************************
+  Setup SPI
+  Open file spidev0.0 (chip enable 0) for read/write access
+  with the file descriptor "fd"
+  Configure transfer speed (1MkHz)
+  ***********************************************************/
    fd = open("/dev/spidev0.0", O_RDWR);
 
    unsigned int speed = 1000000;
@@ -96,15 +82,15 @@ Configure transfer speed (1MkHz)
   printf ("made it past the register initialization...\n");
   cout << "does cout actually work?";
 
-/**********************************************************
-An endless loop that repeatedly sends the demonstration
-commands to the Arduino and displays the results
-***********************************************************/
+  /**********************************************************
+  An endless loop that repeatedly sends simple math problems
+  to the Arduino, captures the results & writes the results to console
+  ***********************************************************/
    while (1)
    {
 
-// this version sends a single byte operand command and two separate two-byte parameters
-// Each execution of sendCommand() transfers 8 bytes in each direction
+  // this version sends a single byte operand command and two separate two-byte parameters
+  // Each execution of sendCommand() transfers 8 bytes in each direction
       results = sendCommand('a', 510, 655);
 
       cout << "Addition results:" << endl;
@@ -117,11 +103,7 @@ commands to the Arduino and displays the results
       cout << "1000 - 250 = " <<  (int)(results) << endl <<endl; 
 
       sleep(1);
-
-
-
    }
-
 }
 
 /**********************************************************
@@ -136,7 +118,6 @@ spiTxRx
  Local variables txDat and rxDat are defined and passed by
  reference.  
 ***********************************************************/
-
 int spiTxRx(unsigned char txDat)
 {
  
@@ -162,8 +143,6 @@ sendCommand
  to send a formatted command sequence/packet to the Arduino
  one byte at and capture the results
 ***********************************************************/
-
-
 int sendCommand(char command, int j, int k)
 {
 
@@ -203,15 +182,14 @@ union resultBuffer_T
   p2Buffer.p2Int = k;
   resultBuffer.resultInt = 0;
 
-/**********************************************************
-An initial handshake sequence sends a one byte start code
-('c') and loops endlessly until it receives the one byte 
-acknowledgment code ('a') and sets the ack flag to true.
-(Note that the loop also sends the command byte while 
-still in handshake sequence to avoid wasting a transmit
-cycle.)
-***********************************************************/
-
+  /**********************************************************
+  An initial handshake sequence sends a one byte start code
+  ('c') and loops endlessly until it receives the one byte 
+  acknowledgment code ('a') and sets the ack flag to true.
+  (Note that the loop also sends the command byte while 
+  still in handshake sequence to avoid wasting a transmit
+  cycle.)
+  ***********************************************************/
   do
   {
     ack = false;
@@ -230,9 +208,9 @@ cycle.)
    }
   while (ack == false);
 
-/**********************************************************
-Send the parameters one byte at a time.
-***********************************************************/
+  /**********************************************************
+  Send the parameters one byte at a time.
+  ***********************************************************/
 
   spiTxRx(p1Buffer.p1Char[0]);
   usleep (10);
@@ -249,11 +227,10 @@ Send the parameters one byte at a time.
   spiTxRx(p2Buffer.p2Char[1]);
   usleep (10);
 
-/**********************************************************
-Push two more zeros through so the Arduino can return the
-results
-***********************************************************/
-
+  /**********************************************************
+  Push two more zeros through so the Arduino can return the
+  results
+  ***********************************************************/
 
   resultByte = spiTxRx(0);
   resultBuffer.resultChar[0] = resultByte;
