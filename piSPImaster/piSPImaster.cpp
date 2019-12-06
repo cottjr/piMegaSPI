5b574e8583a7ff5fafe2b038b15c525a6c596a12
cottjr <carl.ott.jr@gmail.com>
2019-12-05 21:13:37 -0600

    SPI resync tests -> use to verify the reference code resynch function plus additional watchdog timer resynch behavior.  Also shows how, if the slave code does not write to SPDR, by default, Arduino SPI automatically returns received bytes to master on next transfer.

--- piSPImaster/piSPImaster.cpp
+++ piSPImaster/piSPImaster.cpp
@@ -82,6 +82,16 @@ int main (void)
   printf ("made it past the register initialization...\n");
   cout << "does cout actually work?";
 
+unsigned char out1;
+unsigned char out2;
+unsigned char out3;
+unsigned char out4;
+unsigned char out5;
+unsigned char out6;
+unsigned char out7;
+unsigned char out8;
+unsigned char out9;
+
   /**********************************************************
   An endless loop that repeatedly sends simple math problems
   to the Arduino, captures the results & writes the results to console
@@ -96,13 +106,74 @@ int main (void)
       cout << "Addition results:" << endl;
       cout << "510 + 655 = " <<  (int)(results) << endl;
 
-
       results = sendCommand('s', 1000, 250);
 
       cout << "Subtraction results:" << endl;
       cout << "1000 - 250 = " <<  (int)(results) << endl <<endl; 
 
-      sleep(1);
+// The following code with 'out1' thru 'out9', plus an oscilloscope, help show how the resynchronizing functions work
+// Design:
+//  these 'spurious' commands ('c') sent when the Ardunio slave is at marker === 0, trigger the arduinoMathServer slave state machine to enter be entered
+//  this means that the Arduino slave assumes that following transmissions from Pi master are part of the command string
+// Running the Test
+//  place an oscilloscope on the Arduino slave 'ISR active' test point
+//  when there are no 'spurious' command bytes sent over SPI, you'll see the 'ISR active' pulse just 16 times
+// Test 1
+//  move the 'spurious' commands below to be between the 'a' and 's' commands above
+//  observe how adding just 1 'spurious' commands causes a bunch of additional 'ISR active' pulses on the scope, not just 1.
+//  That is because the reference code will continue sending an 'c' and 'command' byte until the Arduino slave returns 'a' to the Pi master
+// Test 2
+//  leave the 'spurious' commands below as they are, just before the 2 second pause
+//  observe that ther are now just 17 pulses visible on the 'ISR active' test point
+//  that's because the 'SPIwdPriorMillis' test automatically resets the state machine in case of any 'spurious' commands that arrive between well formed bursts.
+//  hence, there is no need for the reference code to iterate 'c' and 'command' bytes to reset the Arduino slave state machine
+//  => then, comment out the 'SPIwdPriorMillis' test in the 'ISR (SPI_STC_vect)' function, and rerun the test.
+//  => observe how many more pulses are required for the reference code to help reinitialize the Arduino slave state machine
+// Test 3
+//  Shows how, if the slave code does not write to SPDR, by default, Arduino SPI automatically returns received bytes to master on next transfer.
+//  to observe how Arduino SPI Slave behavior naturally varies based on received bytes,
+//  uncomment & send all 9 'spurious' bytes from the Pi master.
+//  also inspect the corresponding Arduino slave code, to note how it only writes to the SPDR register for the acknowledge 'a' byte, and for the 2 'resultBuffer' bytes
+//  ie. note that for the other bytes in the protocol, the Arduino slave code does not write anything to the SPDR register
+//  when you run this test => observe how certain bytes received by Arduino slave are returned to Pi master on the next transfer
+    out1 = spiTxRx('c');
+    usleep (10);
+
+    // out2 = spiTxRx('p');
+    // usleep (10);
+
+    // out3 = spiTxRx('q');
+    // usleep (10);    
+
+    // out4 = spiTxRx('r');
+    // usleep (10);   
+
+    // out5 = spiTxRx('t');
+    // usleep (10);   
+
+    // out6 = spiTxRx('w');
+    // usleep (10);   
+
+    // out7 = spiTxRx('x');
+    // usleep (10);   
+
+    // out8 = spiTxRx('y');
+    // usleep (10);   
+
+    // out9 = spiTxRx('z');
+    // usleep (10);   
+
+    cout << "out1: " << (int)out1 << "\n";
+    // cout << "out2: " << out2 << "\n";
+    // cout << "out3: " << out3 << "\n";
+    // cout << "out4: " << out4 << "\n";
+    // cout << "out5: " << out5 << "\n";
+    // cout << "out6: " << out6 << "\n";
+    // cout << "out7: " << out7 << "\n";
+    // cout << "out8: " << (int)out8 << "\n";
+    // cout << "out9: " << out9 << "\n";
+
+      sleep(2);
    }
 }
 
